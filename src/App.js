51fe6bacb8fef022587c946ecba3a03f223c07c1
lumsdent <tyler.j.lumsden@gmail.com>
2022-12-02 18:59:29 -0500

    Added sprites

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import React, { useState, useEffect } from "react";
 import Pokedex from "pokedex-promise-v2";
 const options = {
@@ -11,18 +10,15 @@ const P = new Pokedex(options);
 function App() {
 	const [data, setData] = useState([]);
 	const [loaded, setLoaded] = useState(false);
+	const [next, setNext] = useState("");
+	const [previous, setPrevious] = useState("");
 
 	useEffect(() => {
 		let ignore = false;
 		let pokemon = [];
 		const loadData = async () => {
-			try {
-				pokemon = await P.getPokemonsList();
-			} catch (error) {
-				throw error;
-			}
-			setData(pokemon.results);
-			setLoaded(true);
+			pokemon = await P.getPokemonsList({ limit: 20, offset: 0 });
+			loadPokemonData(pokemon);
 		};
 
 		if (!ignore) {
@@ -34,10 +30,46 @@ function App() {
 		};
 	}, []);
 
+	async function loadPokemonData(pokemon) {
+		let pokemonData = [];
+		for (let mon of pokemon.results) {
+			let data = await P.getResource(mon.url);
+			console.log(data);
+			pokemonData.push(data);
+		}
+		setData([...pokemonData]);
+		setNext(pokemon.next);
+		setPrevious(pokemon.previous);
+		setLoaded(true);
+	}
+
+	async function handleClick(url) {
+		setLoaded(false);
+		let pokemon = await P.getResource(url);
+		loadPokemonData(pokemon);
+		window.scrollTo(0, 0);
+	}
+
 	return (
 		<div className="App">
 			<header className="App-header">
-				{loaded && data.map((pokemon) => <h2>{pokemon.name}</h2>)}
+				<button onClick={() => handleClick(previous)}>Previous</button>
+        <button onClick={() => handleClick(next)}>Next</button>
+        <br></br>
+				{loaded &&
+					data.map((pokemon, index) => (
+						<>
+							<img
+								key={index}
+								src={pokemon.sprites.front_default}
+								alt={pokemon.sprites.front_default}
+							></img>
+							<h2 key={pokemon.id}>{pokemon.name}</h2>
+						</>
+					))}
+				<p>test</p>
+				<button onClick={() => handleClick(previous)}>Previous</button>
+				<button onClick={() => handleClick(next)}>Next</button>
 			</header>
 		</div>
 	);
